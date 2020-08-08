fb2aecb5c46e4c7819ec8fefe293ab95575f7d1d
emrah alpaslan <eailovemyjob@gmail.com>
2020-08-08 18:20:50 +0200

    first commit from local repository

---
+++ src/first_package/FirstClass.java
@@ -0,0 +1,13 @@
+package first_package;
+
+public class FirstClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Our first commit from local repo");
+		
+		
+	}
+
+}
