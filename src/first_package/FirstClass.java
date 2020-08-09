c323c39d988b4984041d103ad256ebeaa1ec9255
Emrah Alpaslan <eailovemyjob@gmail.com>
2020-08-09 07:59:36 +0200

    First local commit

--- src/first_package/FirstClass.java
+++ src/first_package/FirstClass.java
@@ -5,13 +5,13 @@ public class FirstClass {
 	public static void main(String[] args) {
 		
 		System.out.println("Our first commit from local repo");
-		System.out.println("Our second commit from local repository");
-
-		System.out.println("Now i am on my alpaslan branch");
-
-		System.out.println("feature to next commit");
-
-		System.out.println("I want to 2.conflict");
+//		System.out.println("Our second commit from local repository");
+//
+//		System.out.println("Now i am on my alpaslan branch");
+//
+//		System.out.println("feature to next commit");
+//
+//		System.out.println("I want to 2.conflict");
 	}
 
 }
