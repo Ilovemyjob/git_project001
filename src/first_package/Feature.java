6b1cbaf6b964bb172ba21fdee33b75d7074b36a4
Emrah Alpaslan <eailovemyjob@gmail.com>
2020-08-09 13:38:35 +0200

    My commit from feature

--- src/first_package/Feature.java
+++ src/first_package/Feature.java
@@ -3,8 +3,8 @@ package first_package;
 public class Feature {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-System.out.println("This is my commit from feature branch");
+		
+		System.out.println("heyyy This is my commit from feature branch");
 	}
 
 }
